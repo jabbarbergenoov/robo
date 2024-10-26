50191e800cf9394bf9208c8e4b3e988e09de70d2
bekbolat <bekbolatjabbarbergenov@gmail.com>
2024-10-26 17:09:15 +0500

    responsive

--- js/scrip.js
+++ js/scrip.js
@@ -4,14 +4,31 @@ const slider = document.querySelector(".slider");
 
 scrollLeftButton.addEventListener("click", () => {
     slider.scrollBy({
-        left: -200, 
-        behavior: 'smooth' 
+        left: -200,
+        behavior: 'smooth'
     });
 });
 
 scrollRightButton.addEventListener("click", () => {
     slider.scrollBy({
-        left: 200, 
-        behavior: 'smooth' 
+        left: 200,
+        behavior: 'smooth'
     });
 });
+
+
+const form = document.querySelector('.registor-right');
+
+form.addEventListener('click', (e) => {
+    e.preventDefault;
+
+    const name = document.querySelector('#name').value;
+    localStorage.setItem('name', name) 
+
+    const phone = document.querySelector('#phone').value;
+    localStorage.setItem('phone', phone) 
+    
+    const mail = document.querySelector('#mail').value;
+    localStorage.setItem('mail', mail) 
+    
+})
